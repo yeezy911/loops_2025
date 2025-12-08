6423e13d40fd32fee66b4d550de2c0c64714d95a
yeezy911 <rmoralez4901@gmail.com>
2025-12-08 19:35:37 +0000

    loops

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -37,10 +37,13 @@
 
 
 
-age = int(input("Enter your age: "))
+# age = int(input("Enter your age: "))
 
-while age < 0:
-    print("Age cant be negative")
-    age = int(input("Enter your age: "))
+# while age < 0:
+#     print("Age cant be negative")
+#     age = int(input("Enter your age: "))
 
-print(f"You are {age} years old")
+# print(f"You are {age} years old")
+
+
+

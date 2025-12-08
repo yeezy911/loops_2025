236b48e318598dfb463438b1ea1f9ae1548d5245
yeezy911 <rmoralez4901@gmail.com>
2025-12-08 19:34:30 +0000

    loops

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,46 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+
+# for loops = execute a block of code a fixed number of times
+#             You can iterate over a range, string, sequence, etc.
+
+
+# credit_card = "1234-5678-9012-3456"
+
+# for x in credit_card
+#     print(x)
+
+
+# for x in range(1, 21):
+#     if x -- 13:
+#         break
+#     else:
+#         print(x)
+
+
+
+
+
+# while loop = executes some code while some condition remain true
+
+
+# name = input("Enter your name")
+
+# while name -- "":
+#     print("You did not enter your name")
+
+# print(f"Hello {name}")
+
+
+
+
+
+
+
+age = int(input("Enter your age: "))
+
+while age < 0:
+    print("Age cant be negative")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old")

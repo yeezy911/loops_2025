423f2ef65d127f66cf26843293177473e0808f04
yeezy911 <rmoralez4901@gmail.com>
2025-12-10 19:32:49 +0000

    looping over string

--- 3_looping_over_string.py
+++ 3_looping_over_string.py
@@ -2,5 +2,14 @@
 # Ask the user for a word.
 # Use a for loop to print each letter on a new line.
 word = input("Whats a random word? ")
+for letter in word:
+    print(letter)
 # Challenge:
 # Count how many vowels are in the word.
+
+vowels = "aeiouAEIOU"
+count = 0
+for letter in word:
+    if letter in vowels:
+        count += 1
+print("Number of vowels:", count)
